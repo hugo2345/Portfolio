19453063ffd703b9e48fec2cd5293fde01e6bdda
hugo <45632541+hugo2345@users.noreply.github.com>
2023-11-09 16:30:20 +0100

    Delete messagerie directory

--- messagerie/require/addMessPub.php
+++
@@ -1,6 +0,0 @@
-<div id="addMessPub">
-    <form method="post" id="formAddMessPub">
-        <input type="text" id="usernameLog" name="usernameLog">
-        <input type="submit" value="connect" class="button" name="buttonConnect" id="buttonConnect">
-    </form>
-</div>
--- messagerie/require/me.php
+++
@@ -1,7 +0,0 @@
-<div id="me">
-    <?php
-    echo "<h2 id='titleMe' style='text-decoration: underline ". $codeCouleur." 5px'> @".$identifiant."</h2>";
-    echo "<h2 id='titleMe'>".$nbMessPub."</h2>";
-    echo "<h2 id='titleMe'>".$nbFriend."</h2>";
-    ?>
-</div>
