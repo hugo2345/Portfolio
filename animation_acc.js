0f6c8c02a431b54e88cfa9713b4447b6b64c2c54
hugo <45632541+hugo2345@users.noreply.github.com>
2023-11-09 17:13:55 +0100

    Update animation_acc.js

--- animation_acc.js
+++ animation_acc.js
@@ -1,27 +1,15 @@
 const moi = document.querySelector("#a_moi"); 
 const moiDiv = document.querySelector("#moi");
 
-/*const evo = document.querySelector("#a_evo");
-const evoDiv = document.querySelector('#evo');*/
-
-// const filtre = document.querySelector("#a_filtre");
-// const filtreDiv = document.querySelector('#filtre');
-
 const projets = document.querySelector("#a_projets"); 
 const projetsDiv = document.querySelector('#projets');
-
-const contact = document.querySelector("#a_contact"); 
-const contactDiv = document.querySelector('#contact');
 
 const body = document.querySelector('body');
 
 function landing (){
     if(body){
         moiDiv.classList.add('hook');
-        /*evoDiv.classList.add('hook');*/
         projetsDiv.classList.add('hook');
-        contactDiv.classList.add('hook');
-        // filtreDiv.classList.add('hook');
     }
 }
 
@@ -35,26 +23,6 @@ function changeColor(){
             moi.classList.remove('backgroundBtnMain');
         }
     })
-
-    /*evo.addEventListener('click', function(){
-        if(evoDiv.classList.contains('hook')){
-            evoDiv.classList.remove('hook');
-            evo.classList.add('backgroundBtnMain');
-        }else{
-            evoDiv.classList.add('hook');
-            evo.classList.remove('backgroundBtnMain');
-        }
-    })*/
-
-    // filtre.addEventListener('click', function(){
-    //     if(filtreDiv.classList.contains('hook')){
-    //         filtreDiv.classList.remove('hook');
-    //         filtre.classList.add('backgroundBtnMain');
-    //     }else{
-    //         filtreDiv.classList.add('hook');
-    //         filtre.classList.remove('backgroundBtnMain');
-    //     }
-    // })
     
     projets.addEventListener('click', function(){
         if(projetsDiv.classList.contains('hook')){
@@ -63,16 +31,6 @@ function changeColor(){
         }else{
             projetsDiv.classList.add('hook');
             projets.classList.remove('backgroundBtnMain');
-        }
-    })
-
-    contact.addEventListener('click', function(){
-        if(contactDiv.classList.contains('hook')){
-            contactDiv.classList.remove('hook');
-            contact.classList.add('backgroundBtnMain');
-        }else{
-            contactDiv.classList.add('hook');
-            contact.classList.remove('backgroundBtnMain');
         }
     })
 }
